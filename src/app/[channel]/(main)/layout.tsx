110176453dc30d029caed3e128a0c390b002a9d5
saranshisatgit <saranshisatgit>
2024-01-12 22:41:32 +0100

    Added Shop Analytics

--- src/app/[channel]/(main)/layout.tsx
+++ src/app/[channel]/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import Script from "next/script";
 import { Footer } from "@/ui/components/Footer";
 import { Header } from "@/ui/components/Header";
 
@@ -10,6 +11,7 @@ export const metadata = {
 export default function RootLayout(props: { children: ReactNode; params: { channel: string } }) {
 	return (
 		<>
+			<Script src="https://analytics.cynsar.capital/js/script.js" data-domain="shop.cicilabel.com" defer />
 			<Header channel={props.params.channel} />
 			<div className="flex min-h-[calc(100dvh-64px)] flex-col">
 				<main className="flex-1">{props.children}</main>
